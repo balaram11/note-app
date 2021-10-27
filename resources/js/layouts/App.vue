bdbcd785cedb96cfc62bc04f65a95c17e111ec5e
balaram11 <balarammadala11@gmail.com>
2021-10-27 10:52:23 -0700

    added vue layout file

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center mt-3">
+            <div class="col-md-8">
+                <div class="card rounded-0">
+                    <div class="card-body">
+                        <!-- TABS -->
+                        <ul class="nav nav-pills mb-3">
+                            <li class="nav-item">
+                                <router-link class="nav-link" :to="">NOTES</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link class="nav-link" :to="">TAGS</router-link>
+                            </li>
+                        </ul>
+
+                        <div>
+                            <router-view></router-view>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
