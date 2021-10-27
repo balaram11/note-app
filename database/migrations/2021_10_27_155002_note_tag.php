5033965f9427b654859549fa08b5cf0b6395d9ba
balaram11 <balarammadala11@gmail.com>
2021-10-27 08:55:33 -0700

    added migrations

---
+++ database/migrations/2021_10_27_155002_note_tag.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class NoteTag extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note_tag', function (Blueprint $table) {
+            $table->foreignId('note_id')->constrained('notes');
+            $table->foreignId('tag_id')->constrained('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note_tag');
+    }
+}
