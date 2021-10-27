887d07934be8a3b329b52d72c36dab1199a701be
balaram11 <balarammadala11@gmail.com>
2021-10-27 09:37:32 -0700

    added tag controller and defined api routes for tag

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tag;
+use App\Http\Requests\TagRequest;
+
+class TagController extends Controller
+{
+     /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getAllTags()
+    {
+        $tags = Tag::get()->toArray();
+
+        return response()->json($tags,200);
+    }
+
+    /**
+     * @param TagRequest $request
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function addTag(TagRequest $request)
+    {
+        $tag = new Tag();
+        $tag->tag_name = $request->get('tagName');
+        $tag->save();
+
+        response()->json(['message' => 'Tag Added Successfully'],200);
+    }
+
+    /**
+     *
+     * @param  int  $tagId
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function deleteTag($tagId)
+    {
+        $tag = Tag::find($id);
+                $tag->delete();
+
+        return response()->json(['message' => 'Deleted Tag Successfully'],200);        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/notes',[NoteController::class,'getAllNotes']);
 Route::post('/add/notes',[NoteController::class,'addNotes']);
+
+Route::get('/tags',[TagController::class,'getAllTags']);
+Route::post('/add/tag',[TagController::class,'addTag']);
+Route::delete('/delete/tag/{id}',[TagController::class,'deleteTag']);
