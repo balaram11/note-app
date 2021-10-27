e18f9fe3281265e0c3f6865cabd1c0f473b64311
balaram11 <balarammadala11@gmail.com>
2021-10-27 10:48:55 -0700

    installed & imported vue router

---
+++ resources/js/router.js
@@ -0,0 +1,14 @@
+/*
+    contains  routes for the application
+ */
+
+/*
+    import vue and vue route
+ */
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export default new VueRouter();
